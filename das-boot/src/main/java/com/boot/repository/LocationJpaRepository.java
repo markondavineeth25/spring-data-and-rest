2fabf0c3558f7202d83ba1e15564ff909e33c1fa
markondavineeth25 <vineeth.markonda@gmail.com>
2019-05-29 07:36:53 +1000

    spring data rest to create hateoas rest responses

---
+++ das-boot/src/main/java/com/boot/repository/LocationJpaRepository.java
@@ -0,0 +1,21 @@
+package com.boot.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import com.boot.model.Location;
+
+@Repository
+@RepositoryRestResource
+public interface LocationJpaRepository extends JpaRepository<Location, Long> {
+	List<Location> findByStateIgnoreCaseStartingWith(String stateName);
+	Location findFirstByStateIgnoreCaseStartingWith(String stateName);
+	List<Location> findByStateNotLikeOrderByStateAsc(String stateName);
+	
+	List<Location> findByStateIsOrCountryEquals(String value, String value2);
+	List<Location> findByStateNot(String state);
+	
+}
